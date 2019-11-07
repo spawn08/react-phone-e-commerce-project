6f0c0642fc389456cce782379e908e20f5d1e02e
spawn08 <amarthkr66@gmail.com>
2019-11-08 00:57:46 +0530

    first commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
-import logo from "../logo.svg";
+import logo from "../bifisheries.png";
 import { ButtonContainer } from "./Button";
 export default class Navbar extends Component {
   render() {
