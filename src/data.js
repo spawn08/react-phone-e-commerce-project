8b482d17975031fa5db3b54d14f1aae52b8cb8e3
spawn08 <amarthkr66@gmail.com>
2019-11-11 10:25:54 +0530

    initial commit

--- src/data.js
+++ src/data.js
@@ -11,7 +11,7 @@ export const storeProducts = [
     price: 50,
     company: "B.I. Fisheries",
     info:
-      "The rohu is a species of fish of the carp family, found in rivers in South Asia. It is a large omnivore and extensively used in aquaculture.",
+      "The rohu is a species of fish of the carp family, found in rivers in South Asia. It is a large omnivore and extensively used in aquaculture. No refund available.",
     inCart: false,
     count: 0,
     total: 0
@@ -23,7 +23,7 @@ export const storeProducts = [
     price: 50,
     company: "B.I. Fisheries",
     info:
-      "Katla, also known as the major South Asian carp, is an economically important South Asian freshwater fish in the carp family Cyprinidae.",
+      "Katla, also known as the major South Asian carp, is an economically important South Asian freshwater fish in the carp family Cyprinidae. No refund available.",
     inCart: false,
     count: 0,
     total: 0
@@ -35,7 +35,7 @@ export const storeProducts = [
     price: 50,
     company: "B.I. Fisheries",
     info:
-      "The mrigal carp, also known as the white carp, is a species of ray-finned fish in the carp family. Native to streams and rivers in India, the only surviving wild population is in the Cauvery River, leading to its IUCN rating as vulnerable.",
+      "The mrigal carp, also known as the white carp, is a species of ray-finned fish in the carp family. Native to streams and rivers in India, the only surviving wild population is in the Cauvery River, leading to its IUCN rating as vulnerable. No refund available.",
     inCart: false,
     count: 0,
     total: 0
@@ -47,7 +47,7 @@ export const storeProducts = [
     price: 55,
     company: "B.I. Fisheries",
     info:
-      "Tilapia is the common name for nearly a hundred species of cichlid fish from the coelotilapine, coptodonine, heterotilapine, oreochromine, pelmatolapiine and tilapiine tribes (formerly all were in Tilapiini), with the economically most important species placed in Coptodonini and Oreochromini.",
+      "Tilapia is the common name for nearly a hundred species of cichlid fish from the coelotilapine, coptodonine, heterotilapine, oreochromine, pelmatolapiine and tilapiine tribes (formerly all were in Tilapiini), with the economically most important species placed in Coptodonini and Oreochromini. No refund available.",
     inCart: false,
     count: 0,
     total: 0
@@ -59,7 +59,7 @@ export const storeProducts = [
     price: 50,
     company: "B.I. Fisheries",
     info:
-      "Microwaved in a flavours ranging from spices, seeds and condiments, this fish recipe is a keeper. Singara is also known as cat fish and kalia means fish curry.",
+      "Microwaved in a flavours ranging from spices, seeds and condiments, this fish recipe is a keeper. Singara is also known as cat fish and kalia means fish curry. No refund available.",
     inCart: false,
     count: 0,
     total: 0
@@ -71,7 +71,7 @@ export const storeProducts = [
     price: 52,
     company: "B.I. Fisheries",
     info:
-      "Size: Free Size \n It Has 1 Piece Of Women's Necklace With 1 Pair Of Earnings. Work: Embellished",
+      "Size: Free Size \n It Has 1 Piece Of Women's Necklace With 1 Pair Of Earnings. Work: Embellished. No refund available.",
     inCart: false,
     count: 0,
     total: 0
@@ -83,7 +83,7 @@ export const storeProducts = [
     price: 55,
     company: "B.I. Fisheries",
     info:
-      "It Has 1 Piece Of Choker With 1 Pair Of Earrings. Work: Embellished. Size: Free Size",
+      "It Has 1 Piece Of Choker With 1 Pair Of Earrings. Work: Embellished. Size: Free Size. No refund available.",
     inCart: false,
     count: 0,
     total: 0
@@ -95,7 +95,7 @@ export const storeProducts = [
     price: 60,
     company: "B.I. Fisheries",
     info:
-      "It Has 1 Piece Of Necklace & 1 Pair Of Earring. Work: Embellished. Size: Free Size",
+      "It Has 1 Piece Of Necklace & 1 Pair Of Earring. Work: Embellished. Size: Free Size. No refund available.",
     inCart: false,
     count: 0,
     total: 0
@@ -107,7 +107,7 @@ export const storeProducts = [
     price: 60,
     company: "B.I. Fisheries",
     info:
-      "It Has 1 Piece Of Choker Necklace With 1 Pair Of Earring & 1 Piece Of Maang Tikka. Work: Beads Work. Size: Free Size",
+      "It Has 1 Piece Of Choker Necklace With 1 Pair Of Earring & 1 Piece Of Maang Tikka. Work: Beads Work. Size: Free Size. No refund available.",
     inCart: false,
     count: 0,
     total: 0
@@ -119,7 +119,7 @@ export const storeProducts = [
     price: 65,
     company: "B.I. Fisheries",
     info:
-      "It Has 1 Piece Of Necklace With 1 Pair Of Earring. Work: Embellished. Size: Free Size",
+      "It Has 1 Piece Of Necklace With 1 Pair Of Earring. Work: Embellished. Size: Free Size. No refund available.",
     inCart: false,
     count: 0,
     total: 0
@@ -131,7 +131,7 @@ export const storeProducts = [
     price: 55,
     company: "B.I. Fisheries",
     info:
-      "It Has 1 Piece of Necklace & 1 Pair of Earrings. Work: Beads Work. Size: Free Size",
+      "It Has 1 Piece of Necklace & 1 Pair of Earrings. Work: Beads Work. Size: Free Size. No refund available.",
     inCart: false,
     count: 0,
     total: 0
@@ -143,7 +143,7 @@ export const storeProducts = [
     price: 65,
     company: "B.I. Fisheries",
     info:
-      "It Has 1 Piece Of Necklace With 1 Pair Of Earring. Work: Embellished. Size: Free Size",
+      "It Has 1 Piece Of Necklace With 1 Pair Of Earring. Work: Embellished. Size: Free Size. No refund available.",
     inCart: false,
     count: 0,
     total: 0
@@ -155,7 +155,7 @@ export const storeProducts = [
     price: 53,
     company: "B.I. Fisheries",
     info:
-      "Multipack: 1, Base Metal: Oxidized Silver. Size: Adjustable",
+      "Multipack: 1, Base Metal: Oxidized Silver. Size: Adjustable. No refund available.",
     inCart: false,
     count: 0,
     total: 0
@@ -167,7 +167,7 @@ export const storeProducts = [
     price: 60,
     company: "B.I. Fisheries",
     info:
-      "It Has 1 Piece Of Necklace & 1 Pair Of Earrings, Multipack: 1. Base Metal: Alloy \n Plating: Gold Plated \n Stone Type: Artificial Beads",
+      "It Has 1 Piece Of Necklace & 1 Pair Of Earrings, Multipack: 1. Base Metal: Alloy \n Plating: Gold Plated \n Stone Type: Artificial Beads. No refund available.",
     inCart: false,
     count: 0,
     total: 0
@@ -179,7 +179,7 @@ export const storeProducts = [
     price: 57,
     company: "B.I. Fisheries",
     info:
-      "It Has 1 Piece Of Necklace With1 Pair Of Earring. Work: Embellished. Size: Free Size",
+      "It Has 1 Piece Of Necklace With1 Pair Of Earring. Work: Embellished. Size: Free Size.No refund available.",
     inCart: false,
     count: 0,
     total: 0
@@ -191,7 +191,7 @@ export const storeProducts = [
     price: 50,
     company: "B.I. Fisheries",
     info:
-      "It Has 1 Piece Of Necklace With 1 Pair Of Earring. Work:  Embellished With Thread Work & Beads Work. Size: Free Size",
+      "It Has 1 Piece Of Necklace With 1 Pair Of Earring. Work:  Embellished With Thread Work & Beads Work. Size: Free Size. No refund available.",
     inCart: false,
     count: 0,
     total: 0
@@ -203,7 +203,7 @@ export const storeProducts = [
     price: 50,
     company: "B.I. Fisheries",
     info:
-      "It Has 1 Piece Of Men's Jeans Type: Stitched, Color: Blue, Fabric: Cotton & Polyurethanes, Size: XS - Waist - 27.56 in, Length - 34.7 in, S - Waist - 28.35 in, Length - 34.9 in, M - Waist - 29.15 in, Length - 35.1 in, L - Waist - 29.92 in, Length - 35.3 in, XL - Waist - 30.71 in, Length - 35.5 in, 2XL - Waist - 31.5 in, Length - 35.68 in, 3XL - Waist - 32.28 in, Length - 35.88 in, ",
+      "It Has 1 Piece Of Men's Jeans Type: Stitched, Color: Blue, Fabric: Cotton & Polyurethanes, Size: XS - Waist - 27.56 in, Length - 34.7 in, S - Waist - 28.35 in, Length - 34.9 in, M - Waist - 29.15 in, Length - 35.1 in, L - Waist - 29.92 in, Length - 35.3 in, XL - Waist - 30.71 in, Length - 35.5 in, 2XL - Waist - 31.5 in, Length - 35.68 in, 3XL - Waist - 32.28 in, Length - 35.88 in. No refund available. ",
     inCart: false,
     count: 0,
     total: 0
@@ -215,7 +215,7 @@ export const storeProducts = [
     price: 60,
     company: "B.I. Fisheries",
     info:
-      "Fabric: Heavy Denim, Waist Size:  30 in, 32 in, 34 in , 36 in, 38 in, Length: Up To 40 in, Type: Stitched, It Has 1 Piece Of Men's  Jeans. Pattern: Solid, Sizes Available - 30, 32, 34, 36, 38.",
+      "Fabric: Heavy Denim, Waist Size:  30 in, 32 in, 34 in , 36 in, 38 in, Length: Up To 40 in, Type: Stitched, It Has 1 Piece Of Men's  Jeans. Pattern: Solid, Sizes Available - 30, 32, 34, 36, 38. No refund available.",
     inCart: false,
     count: 0,
     total: 0
@@ -227,7 +227,247 @@ export const storeProducts = [
     price: 55,
     company: "B.I. Fisheries",
     info:
-      "Fabric: Denim, Waist Size: 28 in, 30 in, 32 in \n Length: Up To 36 in, Type: Stitched, It Has 1 Piece Of Women's Jeans. Pattern: Solid, Sizes Available - 28, 30, 32, 34.",
+      "Fabric: Denim, Waist Size: 28 in, 30 in, 32 in \n Length: Up To 36 in, Type: Stitched, It Has 1 Piece Of Women's Jeans. Pattern: Solid, Sizes Available - 28, 30, 32, 34. No refund available.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+	{
+    id: 20,
+    title: "Trendy Women's Dobby Denim Jean",
+    img: "img/clothes-4.jpg",
+    price: 55,
+    company: "B.I. Fisheries",
+    info:
+      "It Has 1 Piece Of Women's Jean. Fabric: Dobby Denim, Waist Size: 28 in , 30 in , 32 in , 34 in \n Type: Stitched, Sizes Available - 28, 30, 32, 34. No refund available. ",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+	{
+    id: 21,
+    title: "Comfy Denim Men's Jean",
+    img: "img/clothes-5.jpg",
+    price: 60,
+    company: "B.I. Fisheries",
+    info:
+      "It Has 1 Piece Of Men's Jeans. Fabric: Denim, Waist Size: 30 in, 32 in, 34 in, 36 in, 38 in, 40 in \n Length: Up To 42 in, Type: Stitched. Sizes Available - 30, 32, 34, 36, 38, 40. No refund available.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+	{
+    id: 22,
+    title: "Stylish Cotton Men's Pant",
+    img: "img/clothes-6.jpg",
+    price: 50,
+    company: "B.I. Fisheries",
+    info:
+      "It Has 1 Piece Of Men's Pants. Fabric: Cotton, Waist Size: 30 in, 32 in, 34 in, 36 in \n Length: Up To 40 in, Type: Stitched. Pattern: Solid, Sizes Available - 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50, 52. No refund available.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+	{
+    id: 23,
+    title: "Attractive Denim Women Jeans",
+    img: "img/clothes-7.jpg",
+    price: 55,
+    company: "B.I. Fisheries",
+    info:
+      "Fabric: Denim. \n Length: Regular. Sizes Available - 28 in, 30 in, 32 in, 34 in. No refund available.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+	{
+    id: 24,
+    title: "Essential Casual Silky Denim Jean",
+    img: "img/clothes-8.jpg",
+    price: 54,
+    company: "B.I. Fisheries",
+    info:
+      "Fabric: Silky Denim, Waist Size: 30 in, 32 in, 34 in, 36 in \n Length: Up To 40 in, Type: Stitched, It Has 2 Piece Of Men's Jeans. Pattern: Solid, Sizes Available - 28, 30, 32, 34, 36. No refund available.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+	{
+    id: 25,
+    title: "New Casual Pearl Style Jeans For Women",
+    img: "img/clothes-8.jpg",
+    price: 55,
+    company: "B.I. Fisheries",
+    info:
+      "Fabric: Denim. \n Length: Regular. Sizes Available - 26, 28, 30, 32. No refund available.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+	{
+    id: 26,
+    title: "Modern Denim Solid Men's Jean",
+    img: "img/clothes-10.jpg",
+    price: 55,
+    company: "B.I. Fisheries",
+    info:
+      "It Has 1 Piece Of Men's Jean. Fabric: Denim. Sizes Available - 28, 30, 32, 34, 36.Type: Stitched. No refund available.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+	{
+    id: 27,
+    title: "Trendy Cotton Men's Shirts",
+    img: "img/clothes-11.jpg",
+    price: 65,
+    company: "B.I. Fisheries",
+    info:
+      "Fabric: Cotton. Sleeves Length: Long Sleeves. Sizes Available - M, L, XL. Pattern: Printed. No refund available.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+	{
+    id: 28,
+    title: "Attractive  Men Shirts",
+    img: "img/clothes-12.jpg",
+    price: 50,
+    company: "B.I. Fisheries",
+    info:
+      "Fabric: Cotton. Sleeve Length: Long Sleeves. Sizes Available - S, M, L, XL, XXL. Pattern: Printed. No refund available.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+	{
+    id: 29,
+    title: "Trendy Cotton Blended Men's Shirts",
+    img: "img/clothes-13.jpg",
+    price: 65,
+    company: "B.I. Fisheries",
+    info:
+      "Fabric: Cotton Blended. Sleeve Length: Long Sleeves. Sizes Available - M, L, XL. Pattern: Solid. No refund available.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+	{
+    id: 30,
+    title: "Classy Jersey Men's T-Shirts",
+    img: "img/clothes-14.jpg",
+    price: 55,
+    company: "B.I. Fisheries",
+    info:
+      "Fabric: Cotton. Length: Regular. Sizes Available - S, M, L, XL. Sleeve Length: Short Sleeves. Color: White. No refund available.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+	{
+    id: 31,
+    title: "Cotton Stylish Hooded Tshirt",
+    img: "img/clothes-15.jpg",
+    price: 70,
+    company: "B.I. Fisheries",
+    info:
+      "Fabric: Cotton Blended. Sleeves: Sleeves Are Included . Sizes Available - S, M, L, XL. Pattern: Striped. No refund available.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+	{
+    id: 32,
+    title: "Stylish Men T-Shirts",
+    img: "img/clothes-16.jpg",
+    price: 65,
+    company: "B.I. Fisheries",
+    info:
+      "Fabric: Cotton. Color: Multicolor. Sizes Available - S, M, L, XL, XXL. Pattern: Printed. No refund available.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+	{
+    id: 33,
+    title: "Pretty Alloy Bangle",
+    img: "img/necklace-12.jpg",
+    price: 65,
+    company: "B.I. Fisheries",
+    info:
+      "It has 2 pieces of Bangles. Material: Alloy. Size: 2.4, 2.6, 2.8. Work: Embellished and Pearl. No refund available.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+	{
+    id: 34,
+    title: "Fabulous Mulberry Silk Women's Lehenga",
+    img: "img/clothes-17.jpg",
+    price: 80,
+    company: "B.I. Fisheries",
+    info:
+      "It Has 1 Piece Of Women's Lehenga, 1 Piece Of Blouse & 1 Piece Of Dupatta. Length: Lehenga - Up To 41 in. Work/ Pattern: Lehanga - Solid, Blouse - Gotta Patti Work , Dupatta - Lace Work. No refund available.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+	{
+    id: 35,
+    title: "Fabulous Jacquard  Women's Lehenga",
+    img: "img/clothes-18.jpg",
+    price: 65,
+    company: "B.I. Fisheries",
+    info:
+      "It Has 1 Piece Of Women's Lehenga, 1 Piece Of Blouse & 1 Piece Of Dupatta. Fabric: Lehenga - Jacquard, Blouse - Tapeta Silk, Dupatta - Banarasi Jacquard. No refund available.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+	{
+    id: 37,
+    title: "Trendy Alloy Jewellery Set",
+    img: "img/necklace-13.jpg",
+    price: 85,
+    company: "B.I. Fisheries",
+    info:
+      "It Has 1 Piece Of Necklace With 1 Pair Of Earring & 1 Piece Of Maangtikka. No refund available.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+	{
+    id: 38,
+    title: "Diya Elegant Party Wear Alloy Women's Jewellery Sets",
+    img: "img/necklace-14.jpg",
+    price: 80,
+    company: "B.I. Fisheries",
+    info:
+      "It Has 1 Piece Of Women's Necklace & 1 Pair Of Earrings. Work: Embellished. No refund available.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+	{
+    id: 39,
+    title: "Elite Trendy Alloy Women's Jewellery Set",
+    img: "img/necklace-15.jpg",
+    price: 85,
+    company: "B.I. Fisheries",
+    info:
+      "It Has 1 Piece Of Necklace With 1 Pair Of Earring. Work: Beads Work. No refund available.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+	{
+    id: 40,
+    title: "Kalbose Fish",
+    img: "img/kalbose.jpg",
+    price: 85,
+    company: "B.I. Fisheries",
+    info:
+      "They are found in freshwater habitats in the tropics and subtropics of Africa and Asia. No refund available.",
     inCart: false,
     count: 0,
     total: 0
