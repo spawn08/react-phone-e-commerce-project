c18c93dcdfc5897270189c349e98a0e190cf1172
spawn08 <amarthkr66@gmail.com>
2019-11-18 00:42:02 +0530

    initial commit

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -5,7 +5,7 @@ export default function AboutUs() {
     <div className="container mt-5">
       <div className="row">
         <div className="col-10 mx-auto text-center text-title text-capitalize">
-          <h1>B.I. Fisheries</h1>
+          <h1>B.I. Fisheries is incorporated in 2015 which provides various kinds of fish for consumption. We have fresh water lake, where we breed various fish. We are doing this fishery business locally in Jodhpur and expanding in international market.</h1>
         </div>
       </div>
     </div>
