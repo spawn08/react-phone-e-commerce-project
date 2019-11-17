1823c33af73d7ddb41be2f845910bea4cd9dcd6a
spawn08 <amarthkr66@gmail.com>
2019-11-18 00:14:59 +0530

    initial commit

--- src/data.js
+++ src/data.js
@@ -66,408 +66,108 @@ export const storeProducts = [
   },
    {
     id: 6,
-    title: "Elegant Jewellery Set Material: Alloy",
-    img: "img/necklace-1.jpg",
-    price: 52,
-    company: "B.I. Fisheries",
-    info:
-      "Size: Free Size \n It Has 1 Piece Of Women's Necklace With 1 Pair Of Earnings. Work: Embellished. No refund available.",
-    inCart: false,
-    count: 0,
-    total: 0
-  },
-  {
-    id: 7,
-    title: "Attractive Oxidized Metal Women's Jewellery Set",
-    img: "img/necklace-2.jpg",
-    price: 55,
-    company: "B.I. Fisheries",
-    info:
-      "It Has 1 Piece Of Choker With 1 Pair Of Earrings. Work: Embellished. Size: Free Size. No refund available.",
-    inCart: false,
-    count: 0,
-    total: 0
-  },
-  {
-    id: 8,
-    title: "Fashionable Fancy Women's Jewellery Set",
-    img: "img/necklace-3.jpg",
-    price: 60,
-    company: "B.I. Fisheries",
-    info:
-      "It Has 1 Piece Of Necklace & 1 Pair Of Earring. Work: Embellished. Size: Free Size. No refund available.",
-    inCart: false,
-    count: 0,
-    total: 0
-  },
-  {
-    id: 9,
-    title: "Charming Oxidized Silver Jewellery Set",
-    img: "img/necklace-4.jpg",
-    price: 60,
-    company: "B.I. Fisheries",
-    info:
-      "It Has 1 Piece Of Choker Necklace With 1 Pair Of Earring & 1 Piece Of Maang Tikka. Work: Beads Work. Size: Free Size. No refund available.",
-    inCart: false,
-    count: 0,
-    total: 0
-  },
-  {
-    id: 10,
-    title: "Fancy German Silver Jewellery Set",
-    img: "img/necklace-5.jpg",
-    price: 65,
-    company: "B.I. Fisheries",
-    info:
-      "It Has 1 Piece Of Necklace With 1 Pair Of Earring. Work: Embellished. Size: Free Size. No refund available.",
-    inCart: false,
-    count: 0,
-    total: 0
-  },
-  {
-    id: 11,
-    title: "Sia Elegant Alloy Women's Jewellery Sets",
-    img: "img/necklace-4.jpg",
-    price: 55,
-    company: "B.I. Fisheries",
-    info:
-      "It Has 1 Piece of Necklace & 1 Pair of Earrings. Work: Beads Work. Size: Free Size. No refund available.",
-    inCart: false,
-    count: 0,
-    total: 0
-  },
-  {
-    id: 12,
-    title: "Shimmering Oxidized Silver Jewellery Set",
-    img: "img/necklace-7.jpg",
-    price: 65,
-    company: "B.I. Fisheries",
-    info:
-      "It Has 1 Piece Of Necklace With 1 Pair Of Earring. Work: Embellished. Size: Free Size. No refund available.",
-    inCart: false,
-    count: 0,
-    total: 0
-  },
-  {
-    id: 13,
-    title: "Fusion Oxidized Silver Kadaa",
-    img: "img/necklace-8.jpg",
-    price: 53,
-    company: "B.I. Fisheries",
-    info:
-      "Multipack: 1, Base Metal: Oxidized Silver. Size: Adjustable. No refund available.",
-    inCart: false,
-    count: 0,
-    total: 0
-  },
-  {
-    id: 14,
-    title: "Attractive Antique Alloy  jewellery  05",
-    img: "img/necklace-9.jpg",
-    price: 60,
-    company: "B.I. Fisheries",
-    info:
-      "It Has 1 Piece Of Necklace & 1 Pair Of Earrings, Multipack: 1. Base Metal: Alloy \n Plating: Gold Plated \n Stone Type: Artificial Beads. No refund available.",
-    inCart: false,
-    count: 0,
-    total: 0
-  },
-  {
-    id: 15,
-    title: "New Fancy Alloy Women's Jewellery Set",
-    img: "img/necklace-10.jpg",
-    price: 57,
-    company: "B.I. Fisheries",
-    info:
-      "It Has 1 Piece Of Necklace With1 Pair Of Earring. Work: Embellished. Size: Free Size.No refund available.",
-    inCart: false,
-    count: 0,
-    total: 0
-  },
-  {
-    id: 16,
-    title: "Antique Oxidized Silver Jewellery Set",
-    img: "img/necklace-11.jpg",
-    price: 50,
-    company: "B.I. Fisheries",
-    info:
-      "It Has 1 Piece Of Necklace With 1 Pair Of Earring. Work:  Embellished With Thread Work & Beads Work. Size: Free Size. No refund available.",
-    inCart: false,
-    count: 0,
-    total: 0
-  },
-  {
-    id: 17,
-    title: "Fabulous Men's Jeans",
-    img: "img/clothes-1.jpg",
-    price: 50,
-    company: "B.I. Fisheries",
-    info:
-      "It Has 1 Piece Of Men's Jeans Type: Stitched, Color: Blue, Fabric: Cotton & Polyurethanes, Size: XS - Waist - 27.56 in, Length - 34.7 in, S - Waist - 28.35 in, Length - 34.9 in, M - Waist - 29.15 in, Length - 35.1 in, L - Waist - 29.92 in, Length - 35.3 in, XL - Waist - 30.71 in, Length - 35.5 in, 2XL - Waist - 31.5 in, Length - 35.68 in, 3XL - Waist - 32.28 in, Length - 35.88 in. No refund available. ",
-    inCart: false,
-    count: 0,
-    total: 0
-  },
-	{
-    id: 18,
-    title: "Unique Men's Heavy Denim Jean",
-    img: "img/clothes-2.jpg",
-    price: 60,
-    company: "B.I. Fisheries",
-    info:
-      "Fabric: Heavy Denim, Waist Size:  30 in, 32 in, 34 in , 36 in, 38 in, Length: Up To 40 in, Type: Stitched, It Has 1 Piece Of Men's  Jeans. Pattern: Solid, Sizes Available - 30, 32, 34, 36, 38. No refund available.",
-    inCart: false,
-    count: 0,
-    total: 0
-  },
-	{
-    id: 19,
-    title: "Stylish Women's Attractive Denim Jeans",
-    img: "img/clothes-3.jpg",
-    price: 55,
-    company: "B.I. Fisheries",
-    info:
-      "Fabric: Denim, Waist Size: 28 in, 30 in, 32 in \n Length: Up To 36 in, Type: Stitched, It Has 1 Piece Of Women's Jeans. Pattern: Solid, Sizes Available - 28, 30, 32, 34. No refund available.",
-    inCart: false,
-    count: 0,
-    total: 0
-  },
-	{
-    id: 20,
-    title: "Trendy Women's Dobby Denim Jean",
-    img: "img/clothes-4.jpg",
-    price: 55,
-    company: "B.I. Fisheries",
-    info:
-      "It Has 1 Piece Of Women's Jean. Fabric: Dobby Denim, Waist Size: 28 in , 30 in , 32 in , 34 in \n Type: Stitched, Sizes Available - 28, 30, 32, 34. No refund available. ",
-    inCart: false,
-    count: 0,
-    total: 0
-  },
-	{
-    id: 21,
-    title: "Comfy Denim Men's Jean",
-    img: "img/clothes-5.jpg",
-    price: 60,
-    company: "B.I. Fisheries",
-    info:
-      "It Has 1 Piece Of Men's Jeans. Fabric: Denim, Waist Size: 30 in, 32 in, 34 in, 36 in, 38 in, 40 in \n Length: Up To 42 in, Type: Stitched. Sizes Available - 30, 32, 34, 36, 38, 40. No refund available.",
-    inCart: false,
-    count: 0,
-    total: 0
-  },
-	{
-    id: 22,
-    title: "Stylish Cotton Men's Pant",
-    img: "img/clothes-6.jpg",
-    price: 50,
-    company: "B.I. Fisheries",
-    info:
-      "It Has 1 Piece Of Men's Pants. Fabric: Cotton, Waist Size: 30 in, 32 in, 34 in, 36 in \n Length: Up To 40 in, Type: Stitched. Pattern: Solid, Sizes Available - 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50, 52. No refund available.",
-    inCart: false,
-    count: 0,
-    total: 0
-  },
-	{
-    id: 23,
-    title: "Attractive Denim Women Jeans",
-    img: "img/clothes-7.jpg",
-    price: 55,
-    company: "B.I. Fisheries",
-    info:
-      "Fabric: Denim. \n Length: Regular. Sizes Available - 28 in, 30 in, 32 in, 34 in. No refund available.",
-    inCart: false,
-    count: 0,
-    total: 0
-  },
-	{
-    id: 24,
-    title: "Essential Casual Silky Denim Jean",
-    img: "img/clothes-8.jpg",
-    price: 54,
-    company: "B.I. Fisheries",
-    info:
-      "Fabric: Silky Denim, Waist Size: 30 in, 32 in, 34 in, 36 in \n Length: Up To 40 in, Type: Stitched, It Has 2 Piece Of Men's Jeans. Pattern: Solid, Sizes Available - 28, 30, 32, 34, 36. No refund available.",
-    inCart: false,
-    count: 0,
-    total: 0
-  },
-	{
-    id: 25,
-    title: "New Casual Pearl Style Jeans For Women",
-    img: "img/clothes-8.jpg",
-    price: 55,
-    company: "B.I. Fisheries",
-    info:
-      "Fabric: Denim. \n Length: Regular. Sizes Available - 26, 28, 30, 32. No refund available.",
-    inCart: false,
-    count: 0,
-    total: 0
-  },
-	{
-    id: 26,
-    title: "Modern Denim Solid Men's Jean",
-    img: "img/clothes-10.jpg",
-    price: 55,
-    company: "B.I. Fisheries",
-    info:
-      "It Has 1 Piece Of Men's Jean. Fabric: Denim. Sizes Available - 28, 30, 32, 34, 36.Type: Stitched. No refund available.",
-    inCart: false,
-    count: 0,
-    total: 0
-  },
-	{
-    id: 27,
-    title: "Trendy Cotton Men's Shirts",
-    img: "img/clothes-11.jpg",
-    price: 65,
-    company: "B.I. Fisheries",
-    info:
-      "Fabric: Cotton. Sleeves Length: Long Sleeves. Sizes Available - M, L, XL. Pattern: Printed. No refund available.",
-    inCart: false,
-    count: 0,
-    total: 0
-  },
-	{
-    id: 28,
-    title: "Attractive  Men Shirts",
-    img: "img/clothes-12.jpg",
-    price: 50,
-    company: "B.I. Fisheries",
-    info:
-      "Fabric: Cotton. Sleeve Length: Long Sleeves. Sizes Available - S, M, L, XL, XXL. Pattern: Printed. No refund available.",
-    inCart: false,
-    count: 0,
-    total: 0
-  },
-	{
-    id: 29,
-    title: "Trendy Cotton Blended Men's Shirts",
-    img: "img/clothes-13.jpg",
-    price: 65,
-    company: "B.I. Fisheries",
-    info:
-      "Fabric: Cotton Blended. Sleeve Length: Long Sleeves. Sizes Available - M, L, XL. Pattern: Solid. No refund available.",
-    inCart: false,
-    count: 0,
-    total: 0
-  },
-	{
-    id: 30,
-    title: "Classy Jersey Men's T-Shirts",
-    img: "img/clothes-14.jpg",
-    price: 55,
-    company: "B.I. Fisheries",
-    info:
-      "Fabric: Cotton. Length: Regular. Sizes Available - S, M, L, XL. Sleeve Length: Short Sleeves. Color: White. No refund available.",
-    inCart: false,
-    count: 0,
-    total: 0
-  },
-	{
-    id: 31,
-    title: "Cotton Stylish Hooded Tshirt",
-    img: "img/clothes-15.jpg",
-    price: 70,
-    company: "B.I. Fisheries",
-    info:
-      "Fabric: Cotton Blended. Sleeves: Sleeves Are Included . Sizes Available - S, M, L, XL. Pattern: Striped. No refund available.",
-    inCart: false,
-    count: 0,
-    total: 0
-  },
-	{
-    id: 32,
-    title: "Stylish Men T-Shirts",
-    img: "img/clothes-16.jpg",
-    price: 65,
-    company: "B.I. Fisheries",
-    info:
-      "Fabric: Cotton. Color: Multicolor. Sizes Available - S, M, L, XL, XXL. Pattern: Printed. No refund available.",
-    inCart: false,
-    count: 0,
-    total: 0
-  },
-	{
-    id: 33,
-    title: "Pretty Alloy Bangle",
-    img: "img/necklace-12.jpg",
-    price: 65,
-    company: "B.I. Fisheries",
-    info:
-      "It has 2 pieces of Bangles. Material: Alloy. Size: 2.4, 2.6, 2.8. Work: Embellished and Pearl. No refund available.",
-    inCart: false,
-    count: 0,
-    total: 0
-  },
-	{
-    id: 34,
-    title: "Fabulous Mulberry Silk Women's Lehenga",
-    img: "img/clothes-17.jpg",
-    price: 80,
-    company: "B.I. Fisheries",
-    info:
-      "It Has 1 Piece Of Women's Lehenga, 1 Piece Of Blouse & 1 Piece Of Dupatta. Length: Lehenga - Up To 41 in. Work/ Pattern: Lehanga - Solid, Blouse - Gotta Patti Work , Dupatta - Lace Work. No refund available.",
-    inCart: false,
-    count: 0,
-    total: 0
-  },
-	{
-    id: 35,
-    title: "Fabulous Jacquard  Women's Lehenga",
-    img: "img/clothes-18.jpg",
-    price: 65,
-    company: "B.I. Fisheries",
-    info:
-      "It Has 1 Piece Of Women's Lehenga, 1 Piece Of Blouse & 1 Piece Of Dupatta. Fabric: Lehenga - Jacquard, Blouse - Tapeta Silk, Dupatta - Banarasi Jacquard. No refund available.",
-    inCart: false,
-    count: 0,
-    total: 0
-  },
-	{
-    id: 37,
-    title: "Trendy Alloy Jewellery Set",
-    img: "img/necklace-13.jpg",
-    price: 85,
-    company: "B.I. Fisheries",
-    info:
-      "It Has 1 Piece Of Necklace With 1 Pair Of Earring & 1 Piece Of Maangtikka. No refund available.",
-    inCart: false,
-    count: 0,
-    total: 0
-  },
-	{
-    id: 38,
-    title: "Diya Elegant Party Wear Alloy Women's Jewellery Sets",
-    img: "img/necklace-14.jpg",
-    price: 80,
-    company: "B.I. Fisheries",
-    info:
-      "It Has 1 Piece Of Women's Necklace & 1 Pair Of Earrings. Work: Embellished. No refund available.",
-    inCart: false,
-    count: 0,
-    total: 0
-  },
-	{
-    id: 39,
-    title: "Elite Trendy Alloy Women's Jewellery Set",
-    img: "img/necklace-15.jpg",
-    price: 85,
-    company: "B.I. Fisheries",
-    info:
-      "It Has 1 Piece Of Necklace With 1 Pair Of Earring. Work: Beads Work. No refund available.",
-    inCart: false,
-    count: 0,
-    total: 0
-  },
-	{
-    id: 40,
     title: "Kalbose Fish",
     img: "img/kalbose.jpg",
     price: 85,
     company: "B.I. Fisheries",
     info:
       "They are found in freshwater habitats in the tropics and subtropics of Africa and Asia. No refund available.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+   {
+    id: 7,
+    title: "Grass Carp",
+    img: "img/grass_cap.jpg",
+    price: 80,
+    company: "B.I. Fisheries",
+    info:
+      "The grass carp is the species of fish with the largest reported production in aquaculture globally, over five million tonnes per year.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+   {
+    id: 8,
+    title: "Silver Carp",
+    img: "img/silver_carp.jpg",
+    price: 70,
+    company: "B.I. Fisheries",
+    info:
+      "The silver carp is a species of freshwater cyprinid fish, a variety of Asian carp native to China and eastern Siberia.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+   {
+    id: 9,
+    title: "Mangur",
+    img: "img/mangur.jpg",
+    price: 60,
+    company: "B.I. Fisheries",
+    info:
+      "The Mangur is a species of freshwater airbreathing catfish native to Southeast Asia. It is named for its ability to walk and wiggle across dry land, to find food or suitable environments.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+   {
+    id: 10,
+    title: "Pangasius",
+    img: "img/pangacy.jpg",
+    price: 77,
+    company: "B.I. Fisheries",
+    info:
+      "Pangasius is a genus of medium-large to very large shark catfishes native to fresh water in South and Southeast Asia.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+   {
+    id: 11,
+    title: "Lanchi",
+    img: "img/lanchi.jpg",
+    price: 70,
+    company: "B.I. Fisheries",
+    info:
+      "Lanchi is a freshwater catfish of the family Siluridae, native to South and Southeast Asia.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+   {
+    id: 12,
+    title: "Singhi",
+    img: "img/lanchi.jpg",
+    price: 70,
+    company: "B.I. Fisheries",
+    info:
+      "The Singhi is a species of airsac catfish found in India, Bangladesh, Pakistan, Nepal, Sri Lanka, Thailand, Myanmar, and Bhutan.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+   {
+    id: 13,
+    title: "Baam",
+    img: "img/baam.jpg",
+    price: 90,
+    company: "B.I. Fisheries",
+    info:
+      "Baam is a subspecies of eel in the genus Anguilla of the family Anguillidae. It is found throughout the Indian subcontinent and neighbouring regions including the East Indies.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+   {
+    id: 14,
+    title: "Common Carp",
+    img: "img/comman_carp.jpg",
+    price: 75,
+    company: "B.I. Fisheries",
+    info:
+      "The common carp is a widespread freshwater fish of eutrophic waters in lakes and large rivers in Europe and Asia.",
     inCart: false,
     count: 0,
     total: 0
